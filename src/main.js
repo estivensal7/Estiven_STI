693a69b330341019b514d2ff44814babd1f0b09c
estivensal7 <estivensal7@gmail.com>
2021-04-04 17:09:47 -0400

    Basic structure for this application has been built out. Anticipating necessary assets, components, views, and services here.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+// Importing Bootstrap Vue
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Import Bootstrap an BootstrapVue CSS files
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Making BootstrapVue available throughout the project
+Vue.use(BootstrapVue);
+// Install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	render: (h) => h(App),
+}).$mount("#app");
